da7aa10d0fab0513f1e6c00e521d36a4c7fcba62
Xavier <142161233+theprofessorxavier@users.noreply.github.com>
2025-03-06 12:21:42 +1000

    Add Hover Effect

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-const container = document.createElement("div");
-container.setAttribute("class","container");
-document.body.appendChild(container);
-for(let i=0;i<16;i++){
-    const row = document.createElement("div");
-    row.setAttribute("class","row");
-    container.appendChild(row);
-    for(let j=0;j<16;i++){
-        const square = document.createElement("div");
-        square.setAttribute("class", "square");
-        row.appendChild(square);
-    }
+const container = document.querySelector(".container");
+//create squares
+for(let i = 0; i < 256; i++){
+    const square = document.createElement("div");
+    square.setAttribute("class", "square");
+    container.appendChild(square);
 }
+//hover effect
+const squares = document.querySelectorAll(".square");
+squares.forEach((square)=>{
+    square.addEventListener("mouseover", ()=>{
+        square.style.backgroundColor = "red";
+    });
+});
